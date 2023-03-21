203d65ad8ba275ba7fe408075ef46f3f457b4bb5
jshaughnessy24 <jshaughnessy@wpi.edu>
2023-03-21 12:46:44 -0400

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import {Link } from "react-router-dom";
+import React from 'react';
+
+
+export default function NavBar() {
+    
+    
+    return (
+    <div> 
+            <ul className="flex-container">
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/myrequests">My Requests</Link>
+            </li>
+            </ul>
+    </div>
+    )
+ }
