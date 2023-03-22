69a9cad00b8378202dd12beb0f992d2608f402d8
Charles Tang <ctang5@wpi.edu>
2023-03-22 11:21:04 -0400

    userinfo

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Header from "./components/Header";
 import { Routes, Route, BrowserRouter} from "react-router-dom"
 import NavBar from './components/NavBar';
 import React from 'react';
+import FoodPantryProfile from "./FoodPantryProfile";
 
 function App() {
   return (
-    
     <BrowserRouter>
     <Header></Header>
       <NavBar></NavBar>
       
       <Routes>
         <Route path="/" element={<p>Lorem ipsum dolor</p>}/>
-        <Route path="/profile"index element={<p>Profile page</p>} />
+        <Route path="/profile"index element={<FoodPantryProfile></FoodPantryProfile>}/>
         <Route path="/myrequests" element={<p>Request page</p>} />
       </Routes>
-    </BrowserRouter>
+
     
+
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/UserInfo.js
@@ -0,0 +1,25 @@
+export default function UserInfo() {
+  return (
+    <div>
+      <div className="row">
+        <p className="row">Username: </p>
+        <form className="row">
+            <input type="text" />
+        </form>
+      </div>
+      <div className="row">
+        <p className="row">Password: </p>
+        <form className="row">
+            <input type="text" />
+        </form>
+      </div>
+      <div className="row">
+        <p className="row">Zipcode: </p>
+        <form className="row">
+            <input type="text" />
+        </form>
+      </div>
+    </div>
+    
+  );
+}
